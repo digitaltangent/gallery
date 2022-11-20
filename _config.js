3b9b46e65384dcd2e85b829c87f0af081d28270a
digitaltangent <info@digitaltangent.com>
2022-11-20 15:34:53 +0300

    merging from test

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://admin:zidVtnf082Vxx3Ry@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://admin:zidVtnf082Vxx3Ry@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://admin:zidVtnf082Vxx3Ry@cluster0.e6lmfea.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://aleeda:<password>@cluster0.4is5duk.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://aleeda:<password>@cluster0.4is5duk.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://aleeda:<password>@cluster0.4is5duk.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
